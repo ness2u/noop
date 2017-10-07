bbada32477d9c9a8d972160d07780aef5f3393ae
Jon Harkness <jharknes@starbucks.com>
2017-10-07 00:40:21 -0700

    custom status endpoint

--- noop.go
+++ noop.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,28 +17,30 @@ func main() {
 	fmt.Println("get /count")
 	fmt.Println("get /mirror")
 	fmt.Println("get /slow")
+	fmt.Println("get /status?code=500")
 
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/count", countHandler)
 	http.HandleFunc("/mirror", mirrorHandler)
 	http.HandleFunc("/slow", slowHandler)
+	http.HandleFunc("/status", statusHandler)
 
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	addHeaders(r, w)
+	addHeaders(w, r)
 	fmt.Fprint(w, "nothing")
 }
 
 func countHandler(w http.ResponseWriter, r *http.Request) {
-	addHeaders(r, w)
+	addHeaders(w, r)
 	c = c + 1
 	fmt.Fprintf(w, "%d", c)
 }
 
 func mirrorHandler(w http.ResponseWriter, r *http.Request) {
-	addHeaders(r, w)
+	addHeaders(w, r)
 
 	fmt.Printf("make peace with the mirror, and watch yourself change\n")
 	fmt.Fprintf(w, "%s %s\n", r.Method, r.URL)
@@ -50,13 +53,35 @@ func mirrorHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+
+	var err error
+	if err = r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	var status int
+	if statusValues := r.Form["code"]; len(statusValues) > 0 {
+		statusValue := statusValues[0]
+		if status, err = strconv.Atoi(statusValue); err != nil {
+			status = http.StatusBadRequest
+		}
+	} else {
+		status = 418 // i'm a teapot
+	}
+
+	addHeaders(w, r)
+	w.WriteHeader(status)
+	fmt.Fprint(w, "nothing")
+}
+
 func slowHandler(w http.ResponseWriter, r *http.Request) {
-	addHeaders(r, w)
+	addHeaders(w, r)
 	time.Sleep(time.Second * 1)
 	fmt.Fprintf(w, "a slow response")
 }
 
-func addHeaders(r *http.Request, w http.ResponseWriter) {
+func addHeaders(w http.ResponseWriter, r *http.Request) {
 
 	correlationId := r.Header.Get("x-correlationId")
 	if len(correlationId) == 0 {
